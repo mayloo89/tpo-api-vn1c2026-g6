923081e5a119c1217ab7da21e73e33a2a2976226
mayloo89 <sebastiansourigues@gmail.com>
2026-04-23 21:23:04 -0300

    Limpieza: elimino entidades sin uso y agrego UsuarioRequestDTO

---
+++ src/main/java/com/uade/tpo/e_commerce/dto/UsuarioRequestDTO.java
@@ -0,0 +1,18 @@
+package com.uade.tpo.e_commerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UsuarioRequestDTO {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String password;
+    private String role;
+}
