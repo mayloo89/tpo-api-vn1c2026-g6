d28e3cee3bb5f7f60cee56f68d6b978a1d743f20
mayloo89 <sebastiansourigues@gmail.com>
2026-06-22 23:27:26 -0300

    mover configuración de rutas de main.jsx a App.jsx

--- e-commerce-front/src/main.jsx
+++ e-commerce-front/src/main.jsx
@@ -1,41 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import './index.css'
-import Navbar from './components/Navbar.jsx'
-import Home from './components/Home.jsx'
-import ProductList from './components/ProductList.jsx'
-import ProductDetail from './components/ProductDetail.jsx'
-import ProductManagement from './components/ProductManagement.jsx'
-import UserManagement from './components/UserManagement.jsx'
-import Favorite from './components/Favorite.jsx'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
-import Cart from './components/Cart.jsx'
+import App from './App.jsx'
 import store from './store/store.js'
-import SessionBootstrap from './store/sessionBootstrap.js'
-import { ThemeProvider } from './context/ThemeContext.jsx'
 
 createRoot(document.getElementById('root')).render(
-    <StrictMode>
-        <Provider store={store}>
-            <ThemeProvider>
-            <BrowserRouter>
-                <SessionBootstrap />
-                <Navbar />
-                <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/products" element={<ProductList />} />
-                    <Route path="/products/:id" element={<ProductDetail />} />
-                    <Route path="/auth" element={<UserManagement />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route element={<ProtectedRoute />}>
-                        <Route path="/admin/products" element={<ProductManagement />} />
-                        <Route path="/favorites" element={<Favorite />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-            </ThemeProvider>
-        </Provider>
-    </StrictMode>,
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
 )
